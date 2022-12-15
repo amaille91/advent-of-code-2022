5e57b5e9bc25582591068482545fed0c883063e8
Alexandre Maille <al.maille@laposte.net>
2022-12-15 20:17:09 +0100

    almost there for day8 part 1

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,36 +1,53 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Day8 (day8Part1, day8Part2) where
 
-import Data.Map (Map, empty, filterWithKey)
-import Data.Map as Map (foldr, map)
+import Prelude hiding (lookup)
+import Data.Map (Map, empty, filterWithKey, keys, delete, alter, singleton, insert, lookup)
+import Data.Map as Map (foldr, map, size, filter)
 
 type TreeSize = Int
 type NbTree = Int
 type Col = Int
+type Row = Int
 type VisibleTreesAcc = Int
 
 day8Part1 :: [String] -> IO ()
 day8Part1 lines =
-  let trees :: [[Int]] = Prelude.map (Prelude.map (read . show)) lines
-      visibleTrees = scanTrees trees 0 0 empty empty empty 0
+  let trees :: [[Int]] = Prelude.map (Prelude.map (read . (:[]))) lines
+      visibleTrees = scanTrees trees 0 0 0 empty empty empty 0
   in putStrLn (show visibleTrees <> " visible trees")
 
-scanTrees :: [[Int]] -> Col -> TreeSize -> Map Col TreeSize -> Map TreeSize NbTree -> Map Col (Map TreeSize Int) -> Int -> Int
-scanTrees [] currentCol biggestTreeOnRow biggestTreeOnCol potentiallyVisibleTreesFromRight potentiallyVisibleTreesFromBottom acc =
-  let visibleTreesOnCols = Map.foldr (+) 0 (Map.map (Map.foldr (+) 0) potentiallyVisibleTreesFromBottom)
+scanTrees :: [[Int]] -> Row -> Col -> TreeSize -> Map Col TreeSize -> Map (Int, Int) TreeSize -> Map Col (Map (Int, Int) TreeSize) -> Int -> Int
+scanTrees [] currentRow currentCol biggestTreeOnRow biggestTreeOnCol potentiallyVisibleTreesFromRight potentiallyVisibleTreesFromBottom acc =
+  let visibleTreesOnCols = Map.size potentiallyVisibleTreesFromBottom
    in visibleTreesOnCols + acc
-scanTrees ([]:rest) currentCol biggestTreeOnRow biggestTreeOnCol potentiallyVisibleTreesFromRight potentiallyVisibleTreesFromBottom acc =
-  let visibleTreesFromRight = Map.foldr (+) 0 potentiallyVisibleTreesFromRight
-   in scanTrees rest 0 biggestTreeOnCol empty potentiallyVisibleTreesFromBottom (acc + visibleTreesFromRight)
-scanTrees ((tree:otherTrees):rest) currentCol biggestTreeOnRow biggestTreeOnCol potentiallyVisibleTreesFromRight potentiallyVisibleTreesFromBottom acc =
+scanTrees ([]:rest) currentRow currentCol biggestTreeOnRow biggestTreeOnCol potentiallyVisibleTreesFromRight potentiallyVisibleTreesFromBottom acc =
+  let visibleTreesFromRight = Map.size potentiallyVisibleTreesFromRight
+      newPotentiallyVisibleFromBottom = maybe empty (\colMap -> insert currentCol (go colMap (keys potentiallyVisibleTreesFromRight)) potentiallyVisibleTreesFromBottom) (lookup currentCol potentiallyVisibleTreesFromBottom)
+        where
+          go m [] = m
+          go m (k:rest) = go (delete k m) rest
+   in scanTrees rest (currentRow + 1) 0 0 biggestTreeOnCol empty newPotentiallyVisibleFromBottom acc + visibleTreesFromRight
+scanTrees ((tree:otherTrees):rest) currentRow currentCol biggestTreeOnRow biggestTreeOnCol potentiallyVisibleTreesFromRight potentiallyVisibleTreesFromBottom acc =
   let visibleFromLeft = tree > biggestTreeOnRow
       newBiggestTreeOnRow = if visibleFromLeft then tree else biggestTreeOnRow
 
-      visibleFromTop = maybe True (lookup currentCol biggestTreeOnCol >>= (\bigTree -> tree > bigTree))
+      visibleFromTop = maybe True (tree >) $ lookup currentCol biggestTreeOnCol
       newBiggestTreeOnCol = if visibleFromTop then insert currentCol tree biggestTreeOnCol else biggestTreeOnCol
 
-      potentiallyVisibleFromRightUpdated = Map.filterWithKey (> tree) potentiallyVisibleTreesFromRight
-      newpotentiallyVisibleFromRight = maybe (insert tree 1 potentiallyVisibleFromRightUpdated) (lookup currentCol potentiallyVisibleFromRightUpdated >>= (\oldVal -> insert tree (oldVal + 1)))
+      visible = visibleFromLeft || visibleFromTop
+      newAcc = if visible then acc + 1 else acc
+
+      potentiallyVisibleFromRightUpdated = Map.filter (> tree) potentiallyVisibleTreesFromRight
+      newPotentiallyVisibleFromRight = 
+        if visible then potentiallyVisibleFromRightUpdated
+          else insert (currentRow, currentCol) tree potentiallyVisibleFromRightUpdated 
+
+      potentiallyVisibleFromBottomUpdated = maybe potentiallyVisibleTreesFromBottom (\colMap -> insert currentCol (Map.filter (> tree) colMap) potentiallyVisibleTreesFromBottom) (lookup currentCol potentiallyVisibleTreesFromBottom)
+      newPotentiallyVisibleFromBottom = 
+        if visible then potentiallyVisibleFromBottomUpdated
+          else alter (maybe (Just $ singleton (currentRow, currentCol) tree) (\colMap -> Just $ insert (currentRow, currentCol) tree potentiallyVisibleFromRightUpdated)) currentCol potentiallyVisibleFromBottomUpdated
+  in scanTrees (otherTrees:rest) currentRow (currentCol + 1) newBiggestTreeOnRow newBiggestTreeOnCol newPotentiallyVisibleFromRight newPotentiallyVisibleFromBottom newAcc
 
 day8Part2 :: [String] -> IO ()
 day8Part2 (firstLine:rest) = putStrLn "Not yet implemented"
